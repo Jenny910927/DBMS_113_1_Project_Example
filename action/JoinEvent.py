f3127d72847bc6dccc3abe1dec6b789fc350cf10
ChiaYu,Hu <jenny.hu2@student.unsw.edu.au>
2024-11-18 22:43:50 +0800

    Fix JoinEvent: jointime is current time

--- action/JoinEvent.py
+++ action/JoinEvent.py
@@ -1,12 +1,17 @@
 from .Action import Action
+from datetime import datetime
 from DB_utils import join_study_group
 class JoinEvent(Action):
     def exec(self, conn, user):
         print("Join Event")
             
         event_id = self.read_input(conn, "study event id")
-        join_time = self.read_input(conn, "join time (in YYYY-MM-DD HH:MI:SS format)")
+        
 
-        join_study_group(user.get_userid(), event_id, join_time)
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        
+
+        join_study_group(user.get_userid(), event_id, current_time)
 
         conn.send(f'\nJoin study group successfully!\n'.encode('utf-8'))
